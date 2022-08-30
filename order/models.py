42fa74c435030da595fa338a9becacdd5b5d16b1
doc2kim <81610276+doc2kim@users.noreply.github.com>
2022-08-30 19:33:23 +0900

    first commit

---
+++ order/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Shop(models.Model):
+    shop_name = models.CharField(max_length=20)
+    shop_address = models.CharField(max_length=40)
+
+
+class Menu(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    food_name = models.CharField(max_length=20)
+
+
+class Order(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    address = models.CharField(max_length=40)
+    order_date = models.DateTimeField('date ordered')
+    estimated_time = models.IntegerField(default=-1)
+    deliver_finish = models.BooleanField(default=False)
+
+
+class OrderFood(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    food_name = models.CharField(max_length=20)
